37fd82d0edfcd170d14e5f96cf11ff43e8139233
= <=>
2016-02-12 22:01:18 -0600

    miex

---
+++ tarea6/src/tarea6/miExcepcion.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea6;
+
+/**
+ *
+ * @author Nozaki
+ */
+public class miExcepcion extends Exception {
+
+    public miExcepcion(String message) {
+        super(message);
+    }
+    
+}
